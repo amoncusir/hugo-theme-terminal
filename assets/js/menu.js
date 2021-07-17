5874f1fb61a0b69636dac43b0ede27c1564a9233
Aran Moncusí Ramírez <aran.moncusi@wallapop.com>
2021-07-17 19:53:01 +0200

    update menu js!

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -12,11 +12,8 @@
   const menu = document.querySelector(".menu");
   const mobileMenuTrigger = document.querySelector(".menu-trigger");
 
-  // const desktopMenuTrigger = document.querySelector(".menu__sub-inner-more-trigger");
-
-  const desktopSubMenus = document.querySelector('.menu__sub-inner')
-
-  const menuMore = document.querySelector(".menu__sub-inner-more");
+  const desktopSubMenus = document.querySelectorAll('.menu__sub-inner')
+  const menuMore = document.querySelectorAll(".menu__sub-inner-more");
 
   const mobileQuery = getComputedStyle(document.body).getPropertyValue("--phoneWidth");
 
@@ -27,8 +24,30 @@
   const handleMenuClasses = () => {
     mobileMenuTrigger && mobileMenuTrigger.classList.toggle(HIDDEN_CLASS, !isMobile());
     menu && menu.classList.toggle(HIDDEN_CLASS, isMobile());
-    menuMore && menuMore.classList.toggle(HIDDEN_CLASS, !isMobile());
+
+    applyIterations(menuMore, i => i.classList.toggle(HIDDEN_CLASS, !isMobile()));
   };
+
+  function isIterable(obj)
+  {
+    // checks for null and undefined
+    if (obj == null)
+    {
+      return false;
+    }
+    return typeof obj[Symbol.iterator] === 'function';
+  }
+
+  function applyIterations(iterable, fn)
+  {
+    if (isIterable(iterable)) {
+      for (let item of iterable) {
+        fn(item);
+      }
+    } else if (iterable === null) {
+      fn(iterable);
+    }
+  }
 
   function setUpMenuTrigger(menuNode)
   {
@@ -39,7 +58,7 @@
 
       const isMenuHidden = menuContainer.classList.contains(HIDDEN_CLASS);
 
-      menuMore.classList.add(HIDDEN_CLASS);
+      applyIterations(menuMore, i => i.classList.add(HIDDEN_CLASS));
 
       if (isMenuHidden)
       {
@@ -50,21 +69,26 @@
 
   // Set Up ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-  for (let desktopSubMenuNode of desktopSubMenus)
-  {
-    console.log(desktopSubMenuNode);
-    setUpMenuTrigger(desktopSubMenuNode);
-  }
+  // console.log(desktopSubMenus);
+
+  applyIterations(desktopSubMenus, i => setUpMenuTrigger(i));
+
+  // for (let desktopSubMenuNode of desktopSubMenus)
+  // {
+  //   // console.log(desktopSubMenuNode);
+  //   setUpMenuTrigger(desktopSubMenuNode);
+  // }
 
   menu && menu.addEventListener("click", e => e.stopPropagation());
-  menuMore && menuMore.addEventListener("click", e => e.stopPropagation());
+  applyIterations(menuMore, i => i.addEventListener("click", e => e.stopPropagation()));
+  // menuMore && menuMore
 
   handleMenuClasses();
 
   document.body.addEventListener("click", () => {
-    if (!isMobile() && menuMore && !menuMore.classList.contains(HIDDEN_CLASS)) {
-      menuMore.classList.add(HIDDEN_CLASS);
-    } else if (isMobile() && menu && !menu.classList.contains(HIDDEN_CLASS)) {
+    if (!isMobile()) {
+      applyIterations(menuMore, i => i.classList.add(HIDDEN_CLASS));
+    } else if (menu && !menu.classList.contains(HIDDEN_CLASS)) {
       menu.classList.add(HIDDEN_CLASS);
     }
   });
@@ -81,15 +105,15 @@
 
 // Desktop menu
 
-  desktopMenuTrigger &&
-  desktopMenuTrigger.addEventListener("click", e => {
-    e.stopPropagation();
-    menuMore && menuMore.classList.toggle(HIDDEN_CLASS);
-
-    if (menuMore.getBoundingClientRect().right > container.getBoundingClientRect().right) {
-      menuMore.style.left = "auto";
-      menuMore.style.right = 0;
-    }
-  });
+  // desktopMenuTrigger &&
+  // desktopMenuTrigger.addEventListener("click", e => {
+  //   e.stopPropagation();
+  //   menuMore && menuMore.classList.toggle(HIDDEN_CLASS);
+  //
+  //   if (menuMore.getBoundingClientRect().right > container.getBoundingClientRect().right) {
+  //     menuMore.style.left = "auto";
+  //     menuMore.style.right = 0;
+  //   }
+  // });
 
 })(document, window);
